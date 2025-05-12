81784fcebf140a08db67bfb4431985c007f9422a
Leandro Nieto <nleandro0juarg@gmail.com>
2025-05-12 16:49:35 -0300

    Primer commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import requests
+from bs4 import BeautifulSoup
+
+# pip install beautifulsoup4
+
+# URL de la página que queremos analizar
+url = "https://www.infobae.com/deportes/"
+response = requests.get(url)
+
+# Parseamos el contenido HTML
+soup = BeautifulSoup(response.text, "html.parser")
+
+# Extraemos títulos de noticias
+for noticia in soup.find_all("h2", "story-card-hl"):
+    print("Título:", noticia.get_text())
+    #print("Enlace:", noticia["href"])
+    print("=" * 50)
